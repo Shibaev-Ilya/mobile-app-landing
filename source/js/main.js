037bb654efcb66313919b9eb19efe5353cd27b59
Ilya Shibaev <hausdr@gmail.com>
2022-04-11 21:32:01 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.js-navigation__toggle');
+const menu = document.querySelector('.navigation__list');
+const header = document.querySelector('.header-main__wrap');
+
+burger.addEventListener('click', (evt) => {
+  menu.classList.toggle('navigation__list--open');
+  header.classList.toggle('header-main__wrap--open');
+  burger.classList.toggle('navigation__toggle--close');
+
+});
